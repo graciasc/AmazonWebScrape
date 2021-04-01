7558312d14eb8f41ff7dab159978b60a8dce945b
Gracias claude <31932887+graciasc@users.noreply.github.com>
2021-04-01 11:34:44 -0600

    Update index.js
    Web Scraping Amazon Best Seller TV
    (Final)

--- src/index.js
+++ src/index.js
@@ -2,48 +2,61 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-const url =
-  "https://www.amazon.com/Best-Sellers-Electronics-Televisions/zgbs/electronics/172659";
+const url ="https://www.amazon.com/Best-Sellers-Electronics-Televisions/zgbs/electronics/172659";
 const puppeteer = require("puppeteer");
-
 
 app.get("/", async (req, res) => {
   const webScraper = (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-  
+
     //wait until page is loaded
     await page.goto(url, { waitUntil: "networkidle2" });
-    //   await page.screenshot({ path: 'amazon.png' });
-  
+    // wait for selector
+    await page.waitForSelector(".zg-item-immersion");
+
+    // access page DOM
     let data = await page.evaluate(() => {
-      return Array.from(
-        document.querySelectorAll('li[class="zg-item-immersion"]')
-      ).map((d) => {
-        let name = d.querySelector('div[class="p13n-sc-truncated"]');
-        let price = d.querySelector('span[class="p13n-sc-price"]');
-        if (price) {
-          price = d.querySelector('span[class="p13n-sc-price"]').innerHTML;
+      // create array from selector
+      return Array.from(document.querySelectorAll("li.zg-item-immersion")).map(
+        (dataAgg) => {
+
+          let name = dataAgg.querySelector("div.p13n-sc-truncated");
+          let price = dataAgg.querySelector("span.p13n-sc-price");
+
+          //validation
+          if (price) {
+            price = price.innerHTML;
+          }
+          if (name) {
+            name = name.innerHTML;
+          }
+
+          return {
+            name,
+            price,
+          };
         }
-        if (name) {
-          name = d.querySelector('div[class="p13n-sc-truncated"]').innerHTML;
-        }
-  
-        return {
-          name,
-          price,
-        };
-      });
+      );
     });
-  
+// Save the screenshot in directory
+    await page.screenshot({
+      path: "./amazon-screenshot.png", 
+      fullPage: true,
+    });
+
+    // close page
+    await page.close();
+    // close browser
     await browser.close();
-    return data  
 
-
+    return data;
   })();
- console.log(await webScraper)
-  res.json(await webScraper)
+  // log data
+  console.log(await webScraper);
+  // return data as an endpoint
+  res.json(await webScraper);
 });
 app.listen(port, () => {
-  console.log("made it", port);
+  console.log("Successful", port);
 });
